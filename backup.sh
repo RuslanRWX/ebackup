0680be21ff15db9ff6ae23beef55651161ab5417
varg <vargaloid@gmail.com>
2018-04-11 13:32:59 +0300

    Added cron task job in the end of configure backup

--- backup.sh
+++ backup.sh
@@ -150,6 +150,12 @@ echo "Enter password for "$UserParm"@"$IPParm
 cat ~/.ssh/id_rsa.pub | ssh -p${Port} $UserParm@$IPParm "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys"
 }
 
+CronTask () {
+read -p " Please, enter timestamp in cron format [1 1 * * *] " CronTaskDate
+ : ${CronTaskDate:="1 1 * * *"}
+echo " $CronTaskDate root $Path/backup.sh -backup >> /dev/null 2>&1 " >> /etc/crontab
+}
+
 Configure () {
 cd $Path
 
@@ -206,6 +212,9 @@ sed  $sedkey "s/$IPold/bksrv=$IPParm/; s/$UPold/bkusr=$UserParm/; s/$Pold/dbback
 	read -p "	|->  Do you want to create ssh-key?:[YES] " sshkey
 	 : ${sshkey:="YES"}
 if   echo ssh-key is $sshkey | grep -E  "[Yy][Ee][Ss]" ; then { SSHkeygen; } fi 
+	read -p "       |->  Do you want to create backup cron task?:[NO] " CronTaskParm
+         : ${CronTaskParm:="NO"}
+if   echo Cron task job is $CronTaskParm | grep -E "[Yy][Ee][Ss]" ; then CronTask; fi
 
 echo "Success !!!"
 
